cba5ecd25072ecdbdcec232cced830dfe0fba5a8
stefan <st.t.lang@gmx.de>
2015-11-25 11:27:04 +0100

    Hope that the bootstrap of the hypergeometric test is working....

--- R/PathwaysGMT.R
+++ R/PathwaysGMT.R
@@ -46,23 +46,39 @@ testGeneList <- function (x, lst=c() ){
 ## PW = PathwaysGMT obj
 ## l  = amount of genes from the original list that match to the background
 
-bootstrap_phyper <- function(PW, l, n=1000, v, pw_genes ) {
+bootstrap_phyper <- function(PW, l, n=3000, v, f, p  ) {
 	## I need to get l random genes from the background (no lay back)
-	f <- function ( bg, l, pw_genes ){
+	f <- function ( bg, original, l, pw_genes ){
+		#print ( paste( bg, l, pw_genes ) )
 		lst <- sample( bg, l )
 		pos <- bg[is.na(match( bg, pw_genes))==F]
 		OK <- lst[is.na(match( lst, pw_genes ))==F]
-		phyper( length(OK), length(pos), x$bgl - length(pos), l,lower.tail=FALSE  )
+		
+		if ( length(OK) < 2 ) {
+			c(1)
+		}
+		else {
+			c(phyper( length(OK), length(pos), length(bg) - length(pos), l,lower.tail=FALSE  ))
+		}
 	}
-	
-	tmp <- boot(PW$background,f, R=500, l=l,  pw_genes=pw_genes )
-	tmp
+	if ( ! exists( 'boot', where=PW) ) {
+		PW$boot <- list()
+		for ( i in 1:length(names(PW$pw))){
+			PW$boot[[i]] <- list()
+			PW$boot[[i]][[length(names(PW$pw[[i]]))+1]] = -1
+		}
+	}
+	if ( is.null(PW$boot[[f]][[p]]) ) {
+		PW$boot[[f]][[p]] <-  boot(data=PW$background,statistic=f, R=n, l=l, pw_genes=PW$pw[[f]][[p]] )
+	}
+	list( p= length(which(PW$boot[[f]][[p]]$t < v)) )
 }
 
 
 testGeneList.pathwaySearch <- function (x, lst=c() ){
 	ret <- matrix(nrow=0, ncol=9 )
 	colnames(ret) <- c('file', 'pathway', 'descr', 'pVal','adj.P BH', 'bootstrap', 'match', 'total','pw length' )
+	
 	for ( f in 1:length(names(x$pw)) ) {
 		for (p in 1:length( names(x$pw[[f]]) ) ) {
 			pos <- x$background[is.na(match( x$background, x$pw[[f]][[p]]))==F]
@@ -72,7 +88,7 @@ testGeneList.pathwaySearch <- function (x, lst=c() ){
 					t<- phyper( length(OK), length(pos), x$bgl - length(pos), length(lst),lower.tail=FALSE  )
 					bootst <- NA
 					if ( t < 0.05 ) {
-						
+						bootst <- bootstrap_phyper ( x, length(lst), v=t, f=f, p=p )
 					}
 					ret <- rbind(ret, c(
 									x$fn[[f]], 
