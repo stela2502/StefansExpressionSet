58c7e6c27cb5651a5b97ae37cdd3e69b79cfa5b2
stefan <st.t.lang@gmx.de>
2015-11-25 09:53:35 +0100

    Try to implement a bootstrap stat for the phyper call

--- R/PathwaysGMT.R
+++ R/PathwaysGMT.R
@@ -43,21 +43,44 @@ testGeneList <- function (x, lst=c() ){
 	UseMethod('testGeneList', x)
 }
 
+## PW = PathwaysGMT obj
+## l  = amount of genes from the original list that match to the background
+
+bootstrap_phyper <- function(PW, l, n=1000, v, pw_genes ) {
+	## I need to get l random genes from the background (no lay back)
+	f <- function ( bg, l, pw_genes ){
+		lst <- sample( bg, l )
+		pos <- bg[is.na(match( bg, pw_genes))==F]
+		OK <- lst[is.na(match( lst, pw_genes ))==F]
+		phyper( length(OK), length(pos), x$bgl - length(pos), l,lower.tail=FALSE  )
+	}
+	
+	tmp <- boot(PW$background,f, R=500, l=l,  pw_genes=pw_genes )
+	tmp
+}
+
+
 testGeneList.pathwaySearch <- function (x, lst=c() ){
-	ret <- matrix(nrow=0, ncol=8 )
-	colnames(ret) <- c('file', 'pathway', 'descr', 'pVal','adj.P BH', 'match', 'total','pw length' )
+	ret <- matrix(nrow=0, ncol=9 )
+	colnames(ret) <- c('file', 'pathway', 'descr', 'pVal','adj.P BH', 'bootstrap', 'match', 'total','pw length' )
 	for ( f in 1:length(names(x$pw)) ) {
 		for (p in 1:length( names(x$pw[[f]]) ) ) {
 			pos <- x$background[is.na(match( x$background, x$pw[[f]][[p]]))==F]
 			if ( length(pos) > 5 ) {
 				OK <- lst[is.na(match( lst, x$pw[[f]][[p]]))==F]
 				if ( length(OK) > 2 ) {
+					t<- phyper( length(OK), length(pos), x$bgl - length(pos), length(lst),lower.tail=FALSE  )
+					bootst <- NA
+					if ( t < 0.05 ) {
+						
+					}
 					ret <- rbind(ret, c(
 									x$fn[[f]], 
 									names(x$pw[[f]])[p], 
 									attributes(x$pw[[f]])$link[p],  
-									phyper( length(OK), length(pos), x$bgl - length(pos), length(lst),lower.tail=FALSE  ), 
+									t, 
 									0,
+									bootst,
 									length(OK), 
 									length(lst), 
 									length(pos), 
