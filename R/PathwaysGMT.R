027f6ca6be0e3ad070d3141957b11bd82a8451fa
stefan <st.t.lang@gmx.de>
2015-11-25 13:52:32 +0100

    Pathway search should be functional now! I have added a 20x malus to the phyper p value as the bootstrap approach consistentyl produced a ~ 20 fold higher p value for all comparisons.

--- R/PathwaysGMT.R
+++ R/PathwaysGMT.R
@@ -45,21 +45,17 @@ testGeneList <- function (x, lst=c() ){
 
 ## PW = PathwaysGMT obj
 ## l  = amount of genes from the original list that match to the background
+## v how many were mapped against this pathway?
 
-bootstrap_phyper <- function(PW, l, n=3000, v, f, p  ) {
+bootstrap_phyper <- function(PW, l, n=2000, v, file.id, pathway.id  ) {
 	## I need to get l random genes from the background (no lay back)
+	
 	f <- function ( bg, original, l, pw_genes ){
 		#print ( paste( bg, l, pw_genes ) )
 		lst <- sample( bg, l )
-		pos <- bg[is.na(match( bg, pw_genes))==F]
-		OK <- lst[is.na(match( lst, pw_genes ))==F]
-		
-		if ( length(OK) < 2 ) {
-			c(1)
-		}
-		else {
-			c(phyper( length(OK), length(pos), length(bg) - length(pos), l,lower.tail=FALSE  ))
-		}
+		pos <- sum(is.na( pw_genes )==F)
+		OK <- sum( is.na( match(pw_genes, lst ) )==F)
+		OK
 	}
 	if ( ! exists( 'boot', where=PW) ) {
 		PW$boot <- list()
@@ -68,27 +64,40 @@ bootstrap_phyper <- function(PW, l, n=3000, v, f, p  ) {
 			PW$boot[[i]][[length(names(PW$pw[[i]]))+1]] = -1
 		}
 	}
-	if ( is.null(PW$boot[[f]][[p]]) ) {
-		PW$boot[[f]][[p]] <-  boot(data=PW$background,statistic=f, R=n, l=l, pw_genes=PW$pw[[f]][[p]] )
+	if ( is.null(PW$boot[[file.id]][[pathway.id]]) ) {
+		r <- list('R' = n, t <- vector('numeric', n) )
+		range <- 1:PW$bgl
+		pw_genes=match(PW$pw[[file.id]][[pathway.id]], PW$background)
+		for ( i in 1:n ) { r$t[i] <- f( bg=range, l=l, pw_genes =  pw_genes ) }
+		PW$boot[[file.id]][[pathway.id]] <- r
+		#PW$boot[[file.id]][[pathway.id]] <-  boot(data=1:PW$bgl ,statistic=f, R=n, l=l, pw_genes=match(PW$pw[[file.id]][[pathway.id]], PW$background) )
+		#PW$boot[[file.id]][[pathway.id]] <-  boot(data=PW$background,statistic=f, R=n, l=l, pw_genes=PW$pw[[file.id]][[pathway.id]] )
 	}
-	list( p= length(which(PW$boot[[f]][[p]]$t < v)) )
+	r <- length(which(PW$boot[[file.id]][[pathway.id]]$t >= v))
+	if ( r == 0 ) {
+		r =1
+		#list( p= paste(">",( 1 /  PW$boot[[file.id]][[pathway.id]]$R ), sep='' ) ) 
+	}
+	#else {
+		list( p= r /  PW$boot[[file.id]][[pathway.id]]$R )
+	#}
 }
 
 
-testGeneList.pathwaySearch <- function (x, lst=c() ){
+testGeneList.pathwaySearch <- function (x, lst=c(),  bootstrap =F ){
 	ret <- matrix(nrow=0, ncol=9 )
 	colnames(ret) <- c('file', 'pathway', 'descr', 'pVal','adj.P BH', 'bootstrap', 'match', 'total','pw length' )
-	
 	for ( f in 1:length(names(x$pw)) ) {
 		for (p in 1:length( names(x$pw[[f]]) ) ) {
 			pos <- x$background[is.na(match( x$background, x$pw[[f]][[p]]))==F]
 			if ( length(pos) > 5 ) {
-				OK <- lst[is.na(match( lst, x$pw[[f]][[p]]))==F]
+				OK <- lst[is.na(fmatch( lst, x$pw[[f]][[p]]))==F]
 				if ( length(OK) > 2 ) {
-					t<- phyper( length(OK), length(pos), x$bgl - length(pos), length(lst),lower.tail=FALSE  )
+					#t <- bootstrap_phyper ( x, length(lst), v=length(OK), file.id=f, pathway.id=p )$p
+					t<- (phyper( length(OK), length(pos), x$bgl - length(pos), length(lst),lower.tail=FALSE  ) * 20) ## emperical check using the bootsrap phyper is too positive
 					bootst <- NA
-					if ( t < 0.05 ) {
-						bootst <- bootstrap_phyper ( x, length(lst), v=t, f=f, p=p )
+					if ( t < 0.01 && bootstrap ) {
+						bootst <- bootstrap_phyper ( x, length(lst), v=length(OK), file.id=f, pathway.id=p )$p
 					}
 					ret <- rbind(ret, c(
 									x$fn[[f]], 
