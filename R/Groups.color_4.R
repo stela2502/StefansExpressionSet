abb788592c7d3ff083b8e8738f0b558827f6befd
stefanl <stefanl@aurora-scc.int>
2016-12-21 12:30:04 +0100

    colors_4 has been updated to also work on annotation columns.

--- R/Groups.color_4.R
+++ R/Groups.color_4.R
@@ -22,29 +22,32 @@ setMethod('colors_4', signature = c ('StefansExpressionSet'),
 			if ( is.null(colFunc) ){
 				colFunc = function(x) {rainbow(x)}
 			}
-			else if ( ! is.na( match( name, names(x@usedObj[['colorRange']]))) ){
-				x@usedObj[['colorRange']][[name]] <- NULL
+			
+			mix <- function ( col, l ) {
+				if ( l > 10 ){
+					if ( l %% 2 == 0 ) {
+						col <- c(rbind( col[1:(l/2)], col[(l/2+1):l] ))
+					}else {
+						col <- c(rbind( col[1:(l/2+1)], c(col[(l/2+1):l],'white') ))[1:l]
+					}
+				}
+				col
 			}
 			if ( is.na( match( name, names(x@usedObj[['colorRange']])))) {
 				if ( !is.na( match(name, colnames(x@samples)))){
-					x@usedObj[['colorRange']][[name]] <- 
-							colFunc( length(levels( x@samples[, name ])))
+					x@usedObj[['colorRange']][[name]] <- mix(
+							colFunc( length(levels( x@samples[, name ]))) ,
+							length(levels( x@samples[, name ])) 
+					)
 				}
 				else if ( !is.na( match(name, colnames(x@annotation)))){
-					x@usedObj[['colorRange']][[name]] <- 
-							colFunc( length(levels( x@annotation[, name ])))
+					x@usedObj[['colorRange']][[name]] <- mix( 
+							colFunc( length(levels( x@annotation[, name ]))),
+							length(levels( x@annotation[, name ]))
+					)
 				}
 				else {
 					stop( "Sorry this column is nether defined in the samples nor in the annotation table!" )
-				}
-				if ( length(levels( x@samples[, name ])) > 10 ) {
-					l <- length(levels( x@samples[, name ]))
-					col <- x@usedObj[['colorRange']][[name]]
-					if ( l %% 2 == 0 ) {
-						x@usedObj[['colorRange']][[name]] <- c(rbind( col[1:(l/2)], col[(l/2+1):l] ))
-					}else {
-						x@usedObj[['colorRange']][[name]] <- c(rbind( col[1:(l/2+1)], c(col[(l/2+1):l],'white') ))[1:l]
-					}
 				}
 			}
 			x
